6c21d60484a765ebb553421a2730fdf27f2eae7c
AstusTiger <107391458+AstusTiger@users.noreply.github.com>
2022-11-09 20:13:55 +0000

    Deploying to gh-pages from @ AstusTiger/goit-js-hw-09@13104fb6f329a37fbc0689b100e2933d9c430acb 🚀

---
+++ 01-color-switcher.bd7138b6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;function d(){const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}function o(){t.getAttribute("disabled")?(e.setAttribute("disabled","true"),t.removeAttribute("disabled")):(t.setAttribute("disabled","true"),e.removeAttribute("disabled"))}t.addEventListener("click",(()=>{r=setInterval(d,1e3),d(),o()})),e.addEventListener("click",(()=>{clearInterval(r),o()}));
+//# sourceMappingURL=01-color-switcher.bd7138b6.js.map
