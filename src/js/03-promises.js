d81d1dbfd165eda8653587f8413b54799e91a855
AstusTiger <astus.tiger@gmail.com>
2022-11-13 14:20:22 +0200

    try22

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,55 @@
-const form = document.querySelector('.form');
-const delay = form.querySelector('input[name="delay"]');
-const step = form.querySelector('input[name="step"]');
-const amount = form.querySelector('input[name="amount"]');
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+const firstDelayRef = formRef.querySelector('input[name="delay"]');
+const stepRef = formRef.querySelector('input[name="step"]');
+const amountRef = formRef.querySelector('input[name="amount"]');
+
+// formRef.addEventListener('submit', async e => {
+//   e.preventDefault();
+//   const firstDelay = firstDelayRef.value;
+//   const step = stepRef.value;
+//   const amount = amountRef.value;
+
+//   for (let i = 0; i < amount; i++) {
+//     try {
+//       const result = await createPromise(i, i === 0 ? firstDelay : step);
+//       Notiflix.Notify.success(
+//         `Fulfiled promise ${result.position + 1} in ${
+//           +firstDelay + result.position * +step
+//         } ms`
+//       );
+//     } catch (e) {
+//       Notiflix.Notify.failure(
+//         `Reject promise ${e.position + 1} in ${
+//           +firstDelay + e.position * +step
+//         } ms`
+//       );
+//     }
+//   }
+// });
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  const firstDelay = firstDelayRef.value;
+  const step = stepRef.value;
+  const amount = amountRef.value;
+
+  for (let i = 0; i < amount; i++) {
+    const delay = +firstDelay + i * +step;
+    createPromise(i, delay)
+      .then(result =>
+        Notiflix.Notify.success(
+          `Fulfiled promise ${result.position + 1} in ${result.delay} ms`
+        )
+      )
+      .catch(e =>
+        Notiflix.Notify.failure(
+          `Reject promise ${e.position + 1} in ${e.delay} ms`
+        )
+      );
+  }
+});
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
